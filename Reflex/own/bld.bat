bd9e23a568e441ddf808c035e29958139ed7cb69
Limes, Ariel (Contractor) <Ariel_Limes@comcast.com>
2023-12-03 22:20:07 -0300

    Deployment_v2

--- Reflex/own/bld.bat
+++ Reflex/own/bld.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Activate conda Enviroment
-call conda activate RX311
+call activate RX311
 
 REM Update Python packages using pip
 call pip install --upgrade pip
@@ -8,8 +8,9 @@ call pip install -r requirements.txt
 call reflex init
 call reflex export --frontend-only
 rmdir /s /q public
-expand frontend.zip -f:* public
-call conda desactivate
+mkdir public
+tar -xf frontend.zip -C public
+call conda deactivate
 del frontend.zip
 
 
